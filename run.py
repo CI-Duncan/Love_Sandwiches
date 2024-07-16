b33f41c9b28ee8b9dbd4b1973333da7398d982d5
Duncan Thow <duncan.thow@students.codeinstitute.net>
2024-07-16 12:46:25 +0000

    Created failure loop and success break

--- run.py
+++ run.py
@@ -17,24 +17,30 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 def get_sales_data():
     """
-    Get Sales figures input from user
+    Get Sales figures input from user.
     """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, seperated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
+    while True:
 
-    data_str = input("Enter your data here: ")
-    
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
+
+        data_str = input("Enter your data here: ")
+        
+        sales_data = data_str.split(",")
+        
+        if validate_data(sales_data):
+            print("Data is Valid!")
+            break
+
+    return sales_data
 
 def validate_data(values):
     """
     Inside the try, converts all string values into integers.
     Raises ValueError if strings cannot be converted into int,
-    or if there aren't exactly6 values.
+    or if there aren't exactly 6 values.
     """
-    print(values)
     try:
         [int(value) for value in values]
         if len(values) != 6:
@@ -43,10 +49,13 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
-        
+        return False
+
+    return True
+
+data = get_sales_data()        
             
 
-get_sales_data()
 
 
 
